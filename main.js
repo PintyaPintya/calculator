5f4b1a4c44da6fab8cd372a143ac24b73e1bd4c8
PintyaPintya <omkajale09@gmail.com>
2023-07-24 13:32:52 +0530

    basic functions done

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+function operate(a,b,op){
+    let result = 0
+    switch(op){
+        case "+":
+            result = add(a,b)
+            break
+        case "-":
+            result = subtract(a,b)
+            break
+        case "*":
+            result = multiply(a,b)
+            break
+        case "/":
+            result = divide(a,b)
+            break
+    }
+    return result
+}
+
+
+// math functions
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide (a,b){
+    return a/b
+}
